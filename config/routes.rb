64e7a2292dc0167c1d7b7ff230176911d1f7d7a6
Antoine Place <48134202+Aplace74@users.noreply.github.com>
2019-06-05 09:46:33 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:show] do
     collection do
-      get 'dashboard' to: 'users#dashboard', as: :dashboard
+      get 'dashboard', to: 'users#dashboard', as: :dashboard
     end
   end
 
