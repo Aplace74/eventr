53716f65c3467ac90cea1cdab97dd6f1c81daa98
Antoine Place <antoineplace2212@gmail.com>
2019-06-05 11:11:21 +0200

    save event page

--- app/controllers/supplies_controller.rb
+++ app/controllers/supplies_controller.rb
@@ -1,2 +1,7 @@
 class SuppliesController < ApplicationController
+  def index
+    @event = Event.find(params[:event_id])
+    @supplies = policy_scope(Supply).where(event_id: @event.id)
+    authorize @supplies
+  end
 end
