a71ae66d7ace4efd37daa14b133a9afeafa73df3
Antoine Place <antoineplace2212@gmail.com>
2019-06-05 16:32:55 +0200

    contributions#create

--- app/controllers/contributions_controller.rb
+++ app/controllers/contributions_controller.rb
@@ -1,2 +1,12 @@
 class ContributionsController < ApplicationController
+  def create
+    contribution_params = {
+      part: params[:part].to_i,
+      participation_id: params[:participation_id].to_i,
+      supply_id: params[:supply_id].to_i
+    }
+    @contribution = Contribution.new(contribution_params)
+    authorize @contribution
+    @contribution.save
+  end
 end
--- app/controllers/supplies_controller.rb
+++ app/controllers/supplies_controller.rb
@@ -1,11 +1,9 @@
 class SuppliesController < ApplicationController
   def index
     @event = Event.find(params[:event_id])
+    @categories = Category.all
     @contribution = Contribution.new
     @supplies = policy_scope(Supply).where(event_id: @event.id)
     authorize @supplies
-    @feed = @supplies.where(category_id: 2)
-    @drinks = @supplies.where(category_id: 1)
-    @others = @supplies.where(category_id: 3)
   end
 end
