dace600adeff839a0a3df8f6760d223611ce11dc
Antoine Place <antoineplace2212@gmail.com>
2019-06-05 13:15:49 +0200

    first part

--- app/controllers/supplies_controller.rb
+++ app/controllers/supplies_controller.rb
@@ -2,6 +2,9 @@ class SuppliesController < ApplicationController
   def index
     @event = Event.find(params[:event_id])
     @supplies = policy_scope(Supply).where(event_id: @event.id)
+    @feed = @supplies.where(category: 'Nourriture')
+    @drinks = @supplies.where(category: 'Boissons')
+    @others = @supplies.where(category: 'Autres')
     authorize @supplies
   end
 end
