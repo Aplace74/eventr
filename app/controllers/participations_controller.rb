2eaf08d2eb4bad28186f4f5348a827901e6f5c02
Christophe Lepretre <contact@start-me-up.fr>
2019-06-06 17:10:38 +0200

    cancel participation

--- app/controllers/participations_controller.rb
+++ app/controllers/participations_controller.rb
@@ -2,7 +2,12 @@ class ParticipationsController < ApplicationController
     def index
         @participants = policy_scope(Participation)
         @event = Event.find(params[:event_id])
+        @participation = current_user.participations.where(event_id: @event.id).first
     end
-
     
+    def update
+        authorize Participation  
+        Participation.find(params[:id]).toggle(:confirmed).save
+        redirect_to event_participations_path(params[:event_id])
+    end
 end
