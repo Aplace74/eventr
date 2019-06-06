057ef2cb152472ad6f76f64c579735d521fc988c
Maxime Derouet <mxderouet@gmail.com>
2019-06-06 11:09:53 +0200

    organize & attending events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,31 +2,33 @@ class EventsController < ApplicationController
 
 
   def index
-    @events = policy_scope(Event)
+      @events = policy_scope(Event)
+      @user_events = @events.where(user: current_user)
+      @attending = Participation.all.where(user: current_user).map { |p| p.event }
   end
 
 
-    def new
-        @event = Event.new
-        @event.user = current_user
-        authorize @event
-    end
+  def new
+      @event = Event.new
+      @event.user = current_user
+      authorize @event
+  end
 
-    def create
-        @event = Event.new(event_params)
-        @event.user = current_user
-        authorize @event
-        if @event.save
-            redirect_to root_path
-        else
-            render :new
-        end
-    end
+  def create
+      @event = Event.new(event_params)
+      @event.user = current_user
+      authorize @event
+      if @event.save
+          redirect_to root_path
+      else
+          render :new
+      end
+  end
 
-    private
+  private
 
-    def event_params
-        params.require(:event).permit!
-    end
+  def event_params
+      params.require(:event).permit!
+  end
 
 end
