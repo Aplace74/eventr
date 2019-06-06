5dfadd8633711a8367216b07a7bfb18bd5d724ee
Aymeric DHALLUIN <aymeric.dhalluin@gmail.com>
2019-06-06 12:51:12 +0200

    Add organizer in table participation

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,32 +1,35 @@
 class EventsController < ApplicationController
-
-
   def index
     @events = policy_scope(Event)
   end
 
+  def new
+    @event = Event.new
+    @event.user = current_user
+    authorize @event
+  end
 
-    def new
-        @event = Event.new
-        @event.user = current_user
-        authorize @event
+  def create
+    @event = Event.new(event_params)
+    @event.user = current_user
+    authorize @event
+    if @event.save
+      # Create participant organiser
+      Participation.create!(
+        confirmed: true,
+        event_id: @event.id,
+        user_id: @event.user.id,
+        organizer: true
+      )
+      redirect_to root_path
+    else
+      render :new
     end
+  end
 
-    def create
-        @event = Event.new(event_params)
-        @event.user = current_user
-        authorize @event
-        if @event.save
-            redirect_to root_path
-        else
-            render :new
-        end
-    end
+  private
 
-    private
-
-    def event_params
-        params.require(:event).permit!
-    end
-
+  def event_params
+    params.require(:event).permit!
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_04_155044) do
+ActiveRecord::Schema.define(version: 2019_06_06_084145) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 2019_06_04_155044) do
     t.datetime "updated_at", null: false
     t.bigint "event_id"
     t.bigint "user_id"
+    t.boolean "organizer", default: false
     t.index ["event_id"], name: "index_participations_on_event_id"
     t.index ["user_id"], name: "index_participations_on_user_id"
   end
