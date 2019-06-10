92e75c5d8cd3655449959d77c2d99c3572e4a717
Christophe Lepretre <contact@start-me-up.fr>
2019-06-10 09:41:24 +0200

    emails désactivés, events controller et user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  after_create :send_welcome_email
+  # after_create :send_welcome_email
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :events
@@ -14,7 +14,7 @@ class User < ApplicationRecord
 
   private
 
-  # def send_welcome_email
-  #   UserMailer.with(user: self).welcome.deliver_now
-  # end
+  def send_welcome_email
+    UserMailer.with(user: self).welcome.deliver_now
+  end
 end
