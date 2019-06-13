b31ce947011e271ad793e3e1f160769f57c69948
Antoine Place <antoineplace2212@gmail.com>
2019-06-13 11:54:36 +0200

    fix bug

--- app/models/fee.rb
+++ app/models/fee.rb
@@ -2,6 +2,9 @@ class Fee < ApplicationRecord
   belongs_to :participation
   has_many :fee_contributions, dependent: :destroy
 
+  validates :cost, presence: true
+  validates :cost, numericality: { greater_than_or_equal_to: 2 }
+
   def price_per_person
     cost / (fee_contributions.size + 1)
   end
